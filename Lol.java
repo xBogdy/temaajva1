55349ce5721ad22fefbfb71932479a17354b4a62
Bogdan Lp <mr.niculec@gmail.com>
2020-09-07 16:32:08 +0300

    commit initial. 2 fileuri+

---
+++ Lol.java
@@ -0,0 +1,65 @@
+import java.io.IOException;
+
+public class Lol {
+/////
+	public static void main(String[] args) throws IOException {
+    System.out.println("Hello, World!"); 
+    char Charu = 97;
+    int a = 20;
+    boolean mareBool;
+int x = -1;
+if (x<100 && x++>=0) {
+	boolean bully = true;
+System.out.println("Bool1 ="+bully);
+System.out.println("X = "+x);
+}else {
+	boolean bully = false;
+	System.out.println("Bool1 ="+bully);
+	System.out.println("X = "+x);
+}
+if (x<100 & x++ > 0)
+{
+	boolean bully = true;
+	System.out.println("Bool2 ="+bully);
+	System.out.println("X = "+x);
+
+}else {
+	boolean bully = false;
+	System.out.println("Bool2 ="+bully);
+	System.out.println("X = "+x);
+}
+//////
+    
+    
+    
+    if(a>5) {
+mareBool = true;
+
+}else {
+	mareBool = false;
+}
+System.out.println("Rs1:" + (++a + ++a + ++a + a-- -6)); 
+	// 21 + 22 + 23 +23 -6 = 83
+	a = 10;
+System.out.println("Rs2:" + ( a++ + a + a-- + a-- + -- a + a++ - --a + a++ - a-- + a - --a + a++ + ++a));
+	//10 + 11 + 11 + 10 + 8 + 8 - 8 + 8 -9 +8 - 7 + 7 + 9 = 66
+System.out.println("Rs3:" +( a++ + a * a-- + a-- * -- a + a++ - --a * a++ - a-- + a - --a * a++ + ++a));
+    // 10   +   11   ×   11   +   10   ×   8   +   8   -   8   ×   8   -   9   +   8   -   7   ×   7   +   9 = 107,921
+System.out.println("maiMare:"+mareBool)	;
+System.out.println("--------------------------------------------------")	;
+System.out.println("char 1 :"+ Charu);
+
+System.out.println("Introduceti char 2 =");
+
+char Charu2 = (char) System.in.read();
+
+System.out.println("char 2 = "+ Charu2 );
+//char[] Charu3 = new char[5];	
+char Charu3 = 'c';
+System.out.println("char 3 = "+ Charu3);	
+
+	
+	}
+
+}
+
